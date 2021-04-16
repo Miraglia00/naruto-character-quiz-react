92b841ac6ae5de9b9f69a392bc1e937ea4659f5b
Gömböcz Zsolt <zsolt.gombocz00gmail.com>
2021-04-16 17:03:58 +0200

    Updated Main view
    Added Links from router, added kunai selector when hovering a link

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,7 +1,33 @@
+import GameTitle from '../components/GameTitle.js'
+import {Link} from 'react-router-dom'
+import kunai from '../images/kunai.png'
+
 const Main = () => {
     return (
-        <div>
-            <h2>Main Menu</h2>
+        <div className='full-page main-menu full-bg'>
+            <GameTitle />
+            <div className='container'>
+                <div className='links'>
+                <div>
+                    <Link to='/start' className='link'>
+                        <img className='link-img' src={kunai} alt=''></img>
+                        <div style={{display: 'inline-block'}}>Start Game</div>
+                    </Link>
+                </div>
+                <div>
+                    <Link to='/start' className='link smaller'>
+                        <img className='link-img' src={kunai} alt=''></img>
+                        <div style={{display: 'inline-block'}}>Settings</div>
+                    </Link>
+                </div>
+                <div>
+                    <Link to='/start' className='link smaller'>
+                        <img className='link-img' src={kunai} alt=''></img>
+                        <div style={{display: 'inline-block'}}>About</div>
+                    </Link>
+                </div>
+                </div>
+            </div>
         </div>
     )
 }
