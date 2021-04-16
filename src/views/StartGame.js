15c4b8bdff4a4a58a04520cf3c7fbbf1b7960ef5
Gömböcz Zsolt <zsolt.gombocz00gmail.com>
2021-04-16 17:04:48 +0200

    Added basic StartGame view
    There will be a little startgame options, such as number of questions and others

---
+++ src/views/StartGame.js
@@ -0,0 +1,10 @@
+const StartGame = () => {
+    return (
+        <div className='full-page orange-bg'>
+            <div className='container'>
+            </div>
+        </div>
+    )
+}
+
+export default StartGame
