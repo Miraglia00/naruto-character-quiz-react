5d438dfae3d6520c41a1510eee7069b81f7c095c
Gömböcz Zsolt <zsolt.gombocz00gmail.com>
2021-04-16 17:01:57 +0200

    Added GameTitle component

---
+++ src/components/GameTitle.js
@@ -0,0 +1,11 @@
+const GameTitle = () => {
+    return (
+        <div className='title-container'>
+            <h1 className='game-title'>Naruto</h1> <br />
+
+            <h2 className='game-title lower'>Charater Quiz</h2>
+        </div>
+    )
+}
+
+export default GameTitle
