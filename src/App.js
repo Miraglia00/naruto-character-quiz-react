2c6ecae0d4a14cd34d8f719861f58786f3c51483
Gömböcz Zsolt <zsolt.gombocz00gmail.com>
2021-04-16 17:01:24 +0200

    Added router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import Main from './views/Main.js'
+import StartGame from './views/StartGame.js'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className="App">
-      <Main />
-    </div>
+    <Router>
+      <div className='full-page'>
+        <Route path='/' exact render={(props)=>(
+          <Main />
+        )}>
+        </Route>
+
+        <Route path='/start' component={StartGame}></Route>
+      </div>
+    </Router>
   );
 }
 
