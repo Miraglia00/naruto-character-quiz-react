4a94302b3b344ff89fd08a747fd37f15c852ab9f
Gömböcz Zsolt <zsolt.gombocz00gmail.com>
2021-04-18 15:18:07 +0200

    Updated main menu view with tailwind
    added sound too

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,32 +1,36 @@
-import GameTitle from '../components/GameTitle.js'
 import {Link} from 'react-router-dom'
 import kunai from '../images/kunai.png'
+import useSound from 'use-sound'
+import kunai_hover from '../sounds/kunai_hover.wav'
+import {useContext} from 'react'
+import {SettingsContext} from '../states/SettingsContext.js'
 
 const Main = () => {
+    const [getSettings] = useContext(SettingsContext)
+
+    const [play, { stop }] = useSound(kunai_hover, (getSettings.sound) ? {volume: 0.1} : {volume: 0.0});
+
     return (
-        <div className='full-page main-menu full-bg'>
-            <GameTitle />
-            <div className='container'>
-                <div className='links'>
-                <div>
-                    <Link to='/start' className='link'>
-                        <img className='link-img' src={kunai} alt=''></img>
-                        <div style={{display: 'inline-block'}}>Start Game</div>
-                    </Link>
-                </div>
-                <div>
-                    <Link to='/start' className='link smaller'>
-                        <img className='link-img' src={kunai} alt=''></img>
-                        <div style={{display: 'inline-block'}}>Settings</div>
-                    </Link>
-                </div>
-                <div>
-                    <Link to='/start' className='link smaller'>
-                        <img className='link-img' src={kunai} alt=''></img>
-                        <div style={{display: 'inline-block'}}>About</div>
-                    </Link>
-                </div>
-                </div>
+        <div className='flex-grow flex justify-center items-center'>
+            <div className='links'>
+            <div className='mb-3 text-center md:text-left'>
+                <Link to='/start' className='link' onMouseEnter={play} onMouseLeave={stop}>
+                    <img className='h-8 inline-block link-img' src={kunai} alt=''></img>
+                    <div className='text-4xl sm:text-6xl' style={{display: 'inline-block'}}>Start Game</div>
+                </Link>
+            </div>
+            <div className='mb-3 text-center md:text-left'>
+                <Link to='/settings' className='link smaller' onMouseEnter={play} onMouseLeave={stop}>
+                    <img className='h-8 inline-block link-img' src={kunai} alt=''></img>
+                    <div className='text-2xl sm:text-4xl' style={{display: 'inline-block'}}>Settings</div>
+                </Link>
+            </div>
+            <div className='mb-3 text-center md:text-left'>
+                <Link to='/about' className='link smaller' onMouseEnter={play} onMouseLeave={stop}>
+                    <img className='h-8 inline-block link-img' src={kunai} alt=''></img>
+                    <div className='text-2xl sm:text-4xl' style={{display: 'inline-block'}}>About</div>
+                </Link>
+            </div>
             </div>
         </div>
     )
