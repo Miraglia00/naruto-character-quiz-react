7b363364d2314159210f47f00b7ce50810f234f9
Gömböcz Zsolt <zsolt.gombocz00gmail.com>
2021-04-18 15:19:20 +0200

    Added sound component to toggle sounds on page

---
+++ src/components/SoundComponent.js
@@ -0,0 +1,32 @@
+import {useContext} from 'react'
+import {SettingsContext} from '../states/SettingsContext.js'
+
+const SoundComponent = ({changeVol}) => {
+
+    const [getSettings, setSettings] = useContext(SettingsContext)
+
+    const onVolumeChange = () => {
+
+        setSettings({...getSettings, sound: !getSettings.sound})
+    }
+    return (
+        <div className='fixed p-5 fill-current scale-100 right-0' onClick={onVolumeChange}>
+            {
+            (getSettings.sound) ? 
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path
+                strokeLinecap="round" 
+                strokeLinejoin="round" 
+                strokeWidth={2} d="M15.536 8.464a5 5 0 010 7.072m2.828-9.9a9 9 0 010 12.728M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z" />
+                </svg>
+            :
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z" clipRule="evenodd" />
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2" />
+                </svg>
+            }
+        </div>
+    )
+}
+
+export default SoundComponent
