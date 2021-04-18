10e625d993116c58305a839513bb1747846332a5
Gömböcz Zsolt <zsolt.gombocz00gmail.com>
2021-04-18 15:17:00 +0200

    Updated style with tailwind, added views and settingscontext

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import Main from './views/Main.js'
 import StartGame from './views/StartGame.js'
+import Settings from './views/Settings.js'
+import About from './views/About.js'
+import GameTitle from './components/GameTitle.js'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
+import SoundComponent from './components/SoundComponent.js'
+import {SettingsProvider} from './states/SettingsContext.js'
 
 const App = () => {
+
   return (
+    <SettingsProvider>
     <Router>
-      <div className='full-page'>
-        <Route path='/' exact render={(props)=>(
+      <div className='xs:container bg-image tracking-wide h-screen flex flex-col items-stretch'>
+      <GameTitle />
+      <SoundComponent />
+        <Route path='/' exact render={(props) =>(
           <Main />
         )}>
         </Route>
 
+
         <Route path='/start' component={StartGame}></Route>
+        <Route path='/settings' component={Settings}></Route>
+        <Route path='/about' component={About}></Route>
+
+        
       </div>
     </Router>
+    </SettingsProvider>
   );
 }
 
