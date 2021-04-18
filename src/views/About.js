b4b2264c69b0ba292766e89a06d7d4b767aec798
Gömböcz Zsolt <zsolt.gombocz00gmail.com>
2021-04-18 15:17:25 +0200

    Added empty views (WIP)

---
+++ src/views/About.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        <div className='full-page orange-bg'>
+            <div className='container'>
+                asd
+            </div>
+        </div>
+    )
+}
+
+export default About
