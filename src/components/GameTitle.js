69b551f9376bb70443b326588223589109e7419e
Gömböcz Zsolt <zsolt.gombocz00gmail.com>
2021-04-18 15:19:57 +0200

    Updated title with route names too

--- src/components/GameTitle.js
+++ src/components/GameTitle.js
@@ -1,11 +1,37 @@
-const GameTitle = () => {
-    return (
-        <div className='title-container'>
-            <h1 className='game-title'>Naruto</h1> <br />
+import PropTypes from 'prop-types'
+import {useLocation} from 'react-router-dom'
 
-            <h2 className='game-title lower'>Charater Quiz</h2>
+
+const GameTitle = ({title}) => {
+    const loc = useLocation()
+    switch(loc.pathname){
+        case '/start':
+            title = 'Start Game'
+            break
+        case '/settings':
+            title='Settings'
+            break
+        case '/about':
+            title='About'
+            break
+        default:
+            break
+    }
+    return (
+        <div className='self-center text-center pt-5'>
+            <h1 className='text-6xl sm:text-7xl md:text-8xl'>{title}</h1>
+
+            {loc.pathname === '/' ? <h2 className='mt-4 text-base sm:text-xl md:text-2xl'>Character Quiz</h2> : ''}
         </div>
     )
+}
+
+GameTitle.defaultProps = {
+    title: 'Naruto'
+}
+
+GameTitle.prototype = {
+    title: PropTypes.string
 }
 
 export default GameTitle
