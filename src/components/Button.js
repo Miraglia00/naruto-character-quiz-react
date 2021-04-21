81e98359278897dfe8edfcdee8e15f948c14f6fe
Gömböcz Zsolt <zsolt.gombocz00gmail.com>
2021-04-21 11:49:21 +0200

    Added link capability to buttons
    Now we can pass down route and it will change the button to a router-dom link tag

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,25 @@
 import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom'
 
-const Button = ({color, text, onClick}) => {
-    return <button style={{backgroundColor: color}} onClick={onClick}>{text}</button>
+const Button = ({color, text, onClick, border, p, link}) => {
+    return (link) ?
+        <Link to={link} className={`custom-button p-${p} ${(border) ? 'border-2 rounded border-black' : ''}`} style={{backgroundColor: color}}>{text}</Link>
+        :
+        <button className={`custom-button p-${p} ${(border) ? 'border-2 rounded border-black' : ''}`} style={{backgroundColor: color}} onClick={onClick}>{text}</button>
 }
 
 Button.defaultProps = {
     color: 'transparent',
+    p: '2'
 }
 
 Button.prototype = {
     text: PropTypes.string,
     color: PropTypes.string,
-    onClick: PropTypes.func
+    onClick: PropTypes.func,
+    border: PropTypes.bool,
+    p: PropTypes.string,
+    link: PropTypes.string
 }
 
 export default Button
