6c0a04984cad27ebcac5a2bf84e30d48dbb715a4
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2021-04-22 08:38:36 +0200

    Added number of questions if game started

--- src/components/GameTitle.js
+++ src/components/GameTitle.js
@@ -22,9 +22,19 @@ const GameTitle = ({title}) => {
         }
         return title
     }
+
+    const setTitleByQuestion = () => {
+        title = `${getSettings.curr_question}/${getSettings.questions}`
+        return title
+    }
+
     return (
         <div className='self-center text-center pt-5'>
-            <h1 className='text-4xl lg:text-5xl break-words'>{setTitleByLoc()}</h1>
+            <h1 className='text-4xl lg:text-5xl break-words'>
+                {(getSettings.started && loc.pathname == '/game') ? setTitleByQuestion() : setTitleByLoc()}
+                {(getSettings.started && loc.pathname == '/game') ? <br /> : ''}
+                {(getSettings.started && loc.pathname == '/game') ? 'question' : ''}
+            </h1>
 
             {loc.pathname === '/' ? <h2 className='mt-4 text-base sm:text-xl md:text-2xl'>Character Quiz</h2> : ''}
         </div>
