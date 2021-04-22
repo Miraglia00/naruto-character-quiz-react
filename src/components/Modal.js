1790ec72b0405d5c7f95608a5f767c544391b8e4
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2021-04-22 08:39:52 +0200

    Added modal WIP

---
+++ src/components/Modal.js
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom'
+
+const Modal = ({color, text, onClick, border, p, link}) => {
+    return(
+        <div className='' style={{backgroundColor: 'red'}}>
+            zxc
+        </div>
+    )
+}
+
+Modal.defaultProps = {
+ 
+}
+
+Modal.prototype = {
+
+}
+
+export default Modal
