181becf9e2cc721236c347c0bf966105dae37b1b
Gömböcz Zsolt <zsolt.gombocz00gmail.com>
2021-04-23 20:21:11 +0200

    Added 2 Context and a loader and popup comp

--- src/App.js
+++ src/App.js
@@ -7,33 +7,45 @@ import Game from './views/Game.js'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import SoundComponent from './components/SoundComponent.js'
 import {SettingsProvider} from './states/SettingsContext.js'
+import {QuestionsProvider} from './states/QuestionsContext.js'
+import {GameProvider} from './states/GameContext'
 import BackButtonComponent from './components/BackButtonComponent.js'
+import Loader from './components/Loader.js'
+import PopUp from './components/PopUp.js'
 
 const App = () => {
 
   return (
     <SettingsProvider>
+    <GameProvider>
+    <Loader />
+    <PopUp />
     <Router>
       <div className='xs:container bg-image tracking-wide h-screen flex flex-col items-stretch'>
       <GameTitle />
       <SoundComponent />
-      <BackButtonComponent />
+      
 
       <Route path='/' exact render={(props) =>(
-        <Main /> //MAINBE VMIERT HATALMAS A KEPERENYO,VAN SCROLL, KITOLJA A SOUND GOMBOT SZELERE
+        <Main />
       )}>
       </Route>
 
 
-      <Route path='/start' component={StartGame}></Route>
+     
       <Route path='/settings' component={Settings}></Route>
       <Route path='/about' component={About}></Route>
+      <QuestionsProvider>
+        <BackButtonComponent />
+        <Route path='/start' component={StartGame}></Route>
+        <Route path='/game' component={Game}></Route>
+      </QuestionsProvider>
 
-      <Route path='/game' component={Game}></Route>
 
         
       </div>
     </Router>
+    </GameProvider>
     </SettingsProvider>
   );
 }
