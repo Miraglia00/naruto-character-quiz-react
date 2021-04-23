175368e3ebd6bb5e901cdb33620e40e33bc420e7
Gömböcz Zsolt <zsolt.gombocz00gmail.com>
2021-04-23 20:20:41 +0200

    Included function in GET.js

--- src/api/getCharacters.js
+++
@@ -1,7 +0,0 @@
-export function getCharacters(limit) {
-    if(limit > 1){
-        return fetch(`http://localhost:5000/v1/random?limit=${limit}&image=true`)
-        .then(data => data.json())
-    } return false;
-    
-  }
