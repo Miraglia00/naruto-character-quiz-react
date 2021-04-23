099cb3fd3ce8d4d65ca09835a20017e1bb345630
Gömböcz Zsolt <zsolt.gombocz00gmail.com>
2021-04-23 20:25:59 +0200

    Added button color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        btnorange: {
+          500: '#ff8624'
+        }
+      }
+    },
   },
   variants: {
     extend: {},
