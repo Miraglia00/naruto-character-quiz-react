3a1909c9be13dd4bbf13657ed501b8d75cd2bb1c
Gömböcz Zsolt <zsolt.gombocz00gmail.com>
2021-04-23 20:24:20 +0200

    Added sessionstorage save

--- src/states/SettingsContext.js
+++ src/states/SettingsContext.js
@@ -1,14 +1,29 @@
-import {useState, createContext} from 'react'
+import {useState, createContext, useEffect} from 'react'
 
 export const SettingsContext = createContext()
 
 export const SettingsProvider = (props) => {
-    const [getSettings, setSettings] = useState({
-        sound: false,
-        started: false,
-        questions: 1,
-        curr_question: 0
+    const [getSettings, setSettings] = useState(() => {
+        const def_settings = 
+            {
+                sound: false,
+                loader: false,
+                popup: {
+                    show: false,
+                    text: '',
+                    color: '',
+                    text_color: '',
+                    time: 3000
+            }
+        }
+        const local = sessionStorage.getItem('settings')
+        
+        return local ? {...JSON.parse(local), sound: false} : def_settings
     })
+
+    useEffect(() => {
+        sessionStorage.setItem('settings', JSON.stringify(getSettings))
+    }, [getSettings])
 
     return (
         <SettingsContext.Provider value={[getSettings, setSettings]}>
