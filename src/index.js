86a909f6ab84a05849b75db70557fe91ef6b1e3c
Gömböcz Zsolt <zsolt.gombocz00gmail.com>
2021-05-30 20:40:05 +0200

    CSS and moved settings provider upper

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {SettingsProvider} from './states/SettingsContext.js'
 
 ReactDOM.render(
   <React.StrictMode>
+    <SettingsProvider>
     <App />
+    </SettingsProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
