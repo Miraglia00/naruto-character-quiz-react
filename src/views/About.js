0749239d3e5fe9680b4ccb83ed0eec4cf4f7a09d
Gömböcz Zsolt <zsolt.gombocz00gmail.com>
2021-05-30 20:57:02 +0200

    Blank

--- src/views/About.js
+++ src/views/About.js
@@ -1,10 +1,6 @@
 const About = () => {
     return (
-        <div className='full-page orange-bg'>
-            <div className='container'>
-                asd
-            </div>
-        </div>
+        <></>
     )
 }
 
--- src/views/Settings.js
+++ src/views/Settings.js
@@ -1,15 +1,6 @@
 const Settings = () => {
     return (
-        <div className='full-page orange-bg'>
-            <div className='container scroll-bg'>
-                <form className='settings-form'>
-                    <div className='form-control'>
-                        <label>Sounds:</label>
-                        <input type='checkbox' />
-                    </div>
-                </form>
-            </div>
-        </div>
+        <></>
     )
 }
 
