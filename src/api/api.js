16826924e9e16ac3d61de99931b175247e56e984
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2022-01-28 20:38:58 +0100

    Updated api

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const api = () => {
     return axios.create({
-        baseURL: 'http://localhost:5000/v1/',
+        baseURL: 'https://naruto-character-api.herokuapp.com/v1/',
         timeout: 3000
     })
 }
